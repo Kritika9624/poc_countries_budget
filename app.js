8a8d8c84e9b481a74c8aa074611f1b4e3f977f02
Kritika Singh <ksingh@avaloninfosys.com>
2022-08-29 13:33:18 +0530

    added data table for chart

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const str =
-  "United States^2020^3923829;United States^2019^2973920;United States^2018^3286942;China^2020^3622313;China^2019^2394820;China^2018^2297620;Germany^2020^1729224;Germany^2019^789450;Germany^2018^791670;Japan^2020^1666454;Japan^2019^1020720;Japan^2018^986250;France^2020^1334944;France^2019^627190;France^2018^564950;United Kingdom^2020^966407;United Kingdom^2019^541210;United Kingdom^2018^532360;Italy^2020^863785;Italy^2019^374590;Italy^2018^394970;India^2020^620739;India^2019^322300;India^2018^291410;Canada^2020^598434;Canada^2019^362920;Canada^2018^356820;Spain^2020^481945;Spain^2019^262320;Spain^2018^265350;Russia^2020^468651;Russia^2019^309980;Russia^2018^293520;Australia^2020^459546;Australia^2019^271050;Australia^2018^272940";
+  "United States^2020^3923829;United States^2019^2973920;United States^2018^3286942;China^2020^3622313;China^2019^2394820;China^2018^2297620;Germany^2020^1729224;Germany^2019^789450;Germany^2018^791670";
 
 // create 2D array from string above
 const strSplit = str.split(";");
@@ -46,7 +46,7 @@ years.forEach((i) => {
 });
 
 // function to create html table from string
-function createTable(tableData, isDdn = false) {
+function createTable(tableData) {
   // create table
   var containerDiv = document.getElementById("tableContainer");
   if (containerDiv.children.length !== 0) {
@@ -77,7 +77,7 @@ function createTable(tableData, isDdn = false) {
     var row = document.createElement("tr");
     rowData.forEach(function (cellData) {
       var cell = document.createElement("td");
-      
+
       cell.appendChild(document.createTextNode(cellData));
       row.appendChild(cell);
     });
@@ -89,16 +89,72 @@ function createTable(tableData, isDdn = false) {
 
 createTable(arr);
 
-// var rows = document.getElementsByTagName('tr');
-// console.log(rows)
-// for(i = 1; i< rows.length; i++){
-// console.log((rows[i].children[2].innerHTML))
+let arr3 = [];
+for (let i = 0; i < arr.length; i++) {
+  let temp = [], flag = false;
+  for(let k = 0; k <arr3.length; k++){
+    if(arr[i][0] === arr3[k][0]){
+      flag = true;
+      break;
+    }
+  }
+  if(flag === false){
 
-// let numCurr = parseInt(rows[i].children[2].innerHTML);
-//   numCurr = numCurr.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
-//   rows[i].children[2].textContent = numCurr.toString();
+    temp.push(arr[i][0]);
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][0] === arr[j][0]) {
+        temp.push(arr[j][2]);
+      }
+    }
+    arr3.push(temp);
+  }
+}
 
-// }
+function createDataTable(tableData) {
+  // create table
+  var containerDiv = document.getElementById("tableContainer3");
+  if (containerDiv.children.length !== 0) {
+    containerDiv.removeChild(containerDiv.firstElementChild);
+  }
+  var table = document.createElement("table");
+  table.id = "datatable3";
+  let thead = document.createElement("thead");
+  var tableBody = document.createElement("tbody");
+
+  let row_1 = document.createElement("tr");
+  let heading_1 = document.createElement("th");
+  // heading_1.innerHTML = "country";
+  let heading_2 = document.createElement("th");
+  heading_2.innerHTML = "2018";
+  let heading_3 = document.createElement("th");
+  heading_3.innerHTML = "2019";
+  let heading_4 = document.createElement("th");
+  heading_4.innerHTML = "2020";
+  
+  row_1.appendChild(heading_1);
+  row_1.appendChild(heading_2);
+  row_1.appendChild(heading_3);
+  row_1.appendChild(heading_4);
+  thead.appendChild(row_1);
+  table.appendChild(thead);
+
+  tableData.forEach(function (rowData) {
+    var row = document.createElement("tr");
+    rowData.forEach(function (cellData) {
+      var cell = document.createElement("td");
+
+      cell.appendChild(document.createTextNode(cellData));
+      row.appendChild(cell);
+    });
+    tableBody.appendChild(row);
+  });
+  table.appendChild(tableBody);
+  document.getElementById("tableContainer3").appendChild(table);
+  document.getElementById('datatable3').style.display='none';
+}
+
+createDataTable(arr3)
+
 
 // choices code starts here
 
@@ -126,9 +182,8 @@ var yearDropdown = new Choices("#yearSelect", {
   placeholderValue: "Select year",
 });
 
-
 // called when filters are added
-let res = [];
+let res = [], tempRes = [];
 countryDropdown.passedElement.element.addEventListener(
   "addItem",
   (event) => {
@@ -139,7 +194,14 @@ countryDropdown.passedElement.element.addEventListener(
       .map((item, index) => {
         res.push(item);
       });
+    arr3
+      .filter((c) => countryValue === c[0])
+      .map((item, index) => {
+        tempRes.push(item);
+      });
     createTable(res);
+    createDataTable(tempRes);
+    
   },
   false
 );
@@ -155,8 +217,6 @@ countryDropdown.passedElement.element.addEventListener(
       .map((item) => {
         removedArr.push(item);
       });
-    console.log(res);
-    console.log(removedArr);
     res = removedArr;
     if (removedArr.length === 0) {
       createTable(arr);
@@ -170,37 +230,34 @@ countryDropdown.passedElement.element.addEventListener(
 
 
 
-function sumOfBudget(){
-  
-}
-
-const columnChartObject = () =>{ 
- const chart = Highcharts.chart({
+const columnChartObject = () => {
+  const chart = Highcharts.chart({
     chart: {
       renderTo: "chartContainer",
       type: "column",
     },
     data: {
-      table: "datatable",
+      table: "datatable3",
     },
     title: {
       text: "Country Budget",
     },
     xAxis: {
       title: {
-        text: 'Country',
-      }
+        text: "Country",
+      },
+      // categories:uniqueCountries,
     },
     yAxis: {
       title: {
         text: "Budget",
       },
     },
+    series: [],
   });
-console.log(chart)
-}
+};
 
-const barChartObject = () =>{ 
+const barChartObject = () => {
   Highcharts.chart({
     chart: {
       renderTo: "chartContainer",
@@ -214,8 +271,8 @@ const barChartObject = () =>{
     },
     xAxis: {
       title: {
-        text: 'Country',
-      }
+        text: "Country",
+      },
     },
     yAxis: {
       title: {
@@ -223,10 +280,9 @@ const barChartObject = () =>{
       },
     },
   });
+};
 
-}
-
-const pieChartObject = () =>{ 
+const pieChartObject = () => {
   Highcharts.chart({
     chart: {
       renderTo: "chartContainer",
@@ -240,8 +296,8 @@ const pieChartObject = () =>{
     },
     xAxis: {
       title: {
-        text: 'Country',
-      }
+        text: "Country",
+      },
     },
     yAxis: {
       title: {
@@ -249,19 +305,16 @@ const pieChartObject = () =>{
       },
     },
   });
-
-}
-
+};
 
 function createColChart() {
-document.addEventListener("DOMContentLoaded", columnChartObject())
+  document.addEventListener("DOMContentLoaded", columnChartObject());
 }
 function createBarChart() {
-document.addEventListener("click", barChartObject())
+  document.addEventListener("click", barChartObject());
 }
 function createPieChart() {
-document.addEventListener("click", pieChartObject())
+  document.addEventListener("click", pieChartObject());
 }
 
-document.getElementById('col').addEventListener('click', createColChart());
-
+document.getElementById("col").addEventListener("click", createColChart());
